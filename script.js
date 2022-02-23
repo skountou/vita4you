f4cce0258471caa428fcde5df635722cafe327b7
skountou <sotiris.kountouriotis@hotmail.com>
2022-02-23 11:11:04 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// $(document).ready(() => {
+//     $('.carousel').slick({
+//     dots: true,
+//     arrows: true,
+//     infinite: true,
+//     slidesToShow: 4 , 
+//     slidesToScroll:1 
+//     });
+// });
